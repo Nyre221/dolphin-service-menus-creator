7558079c934945bb1d4d2627c1333bf40cb6e55a
Nyre221 <104171042+Nyre221@users.noreply.github.com>
2022-12-12 02:45:24 +0100

    removed list of available qt translations

--- translation_manager.py
+++ translation_manager.py
@@ -1,10 +1,8 @@
 import importlib.resources
-import translations
+import translations.qt_translations
+from translations.qt_translations import available_qt_translations
 from translations.placeholder_translations import available_placeholder_translations
 import locale
-
-available_qt_translation = ["it_IT", "nl_NL"]
-
 
 class Translator:
 
@@ -25,10 +23,10 @@ class Translator:
         # since there are two translation systems, unfortunately I have to manage qt separately.
             # check if the translation file for the current locale exists
         # if available_qt_translation.get(self.system_locale, None) != None:
-        if self.system_locale in available_qt_translation:
+        if self.system_locale in available_qt_translations:
             # load the translation from a package. Needed because if the program is zipped with zipapp
             # the translation cannot be loaded in the normal way.
-            translation = importlib.resources.read_binary(translations, self.system_locale + ".qm")
+            translation = importlib.resources.read_binary(translations.qt_translations, self.system_locale + ".qm")
             return translation
         else:
             # because the english words are hardcoded, there is not a translation file to return.
