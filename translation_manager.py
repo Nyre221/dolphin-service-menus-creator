1634194da6f7b487c1bcb0d831ecb892a19c74f1
Nyre221 <104171042+Nyre221@users.noreply.github.com>
2022-12-13 17:24:10 +0100

    Now use the dictionaries to get the location of the translation.

--- translation_manager.py
+++ translation_manager.py
@@ -3,16 +3,16 @@ import translations.qt_translations
 from translations.qt_translations import available_qt_translations
 from translations.placeholder_translations import available_placeholder_translations
 import locale
+import os
 
 class Translator:
 
     def __init__(self):
-        self.system_locale = locale.getlocale()[0]
-
+        self.system_language = os.getenv("LANG").split(".")[0]
 
     def translate(self, string: str):
         # get the dictionary of the current locale if exist or return the default one otherwise
-        locale_dictionary = available_placeholder_translations.get(self.system_locale, available_placeholder_translations["en_US"])
+        locale_dictionary = available_placeholder_translations.get(self.system_language, available_placeholder_translations["en_US"])
         # return the translated string or the default one in case the string doesn't exist.
         translated_string = locale_dictionary.get(string, available_placeholder_translations["en_US"][string])
         return translated_string
@@ -22,11 +22,10 @@ class Translator:
     def get_qt_translation(self):
         # since there are two translation systems, unfortunately I have to manage qt separately.
             # check if the translation file for the current locale exists
-        # if available_qt_translation.get(self.system_locale, None) != None:
-        if self.system_locale in available_qt_translations:
+        if available_qt_translations.get(self.system_language,None) != None:
             # load the translation from a package. Needed because if the program is zipped with zipapp
             # the translation cannot be loaded in the normal way.
-            translation = importlib.resources.read_binary(translations.qt_translations, self.system_locale + ".qm")
+            translation = importlib.resources.read_binary(translations.qt_translations,available_qt_translations[self.system_language])
             return translation
         else:
             # because the english words are hardcoded, there is not a translation file to return.
