e83cfd90f322e5006169f111e26290ce3f9966cc
Nyre221 <104171042+Nyre221@users.noreply.github.com>
2022-12-13 17:22:03 +0100

    Add files via upload

--- translations/qt_translations/__init__.py
+++ translations/qt_translations/__init__.py
@@ -1,11 +1,5 @@
-from translations.placeholder_translations import en_US
-from translations.placeholder_translations import it_IT
-from translations.placeholder_translations import nl_NL
-
-
-available_placeholder_translations = {
-    "it_IT":it_IT.dictionary,
-    "en_US":en_US.dictionary,
-    "nl_NL":nl_NL.dictionary,
-    "de_DE":nl_NL.dictionary,
+available_qt_translations = {
+    "it_IT":"it_IT.qm",
+    "nl_NL":"nl_NL.qm",
+    "de_DE":"nl_NL.qm",
 }
