1a73503abf2d117270ef69bbc6fd5e14829255c2
Nyre221 <104171042+Nyre221@users.noreply.github.com>
2022-12-13 17:19:34 +0100

    List of available languages converted into dictionary.

--- translations/qt_translations/__init__.py
+++ translations/qt_translations/__init__.py
@@ -1,1 +1,11 @@
-available_qt_translations = ["it_IT", "nl_NL"]
+from translations.placeholder_translations import en_US
+from translations.placeholder_translations import it_IT
+from translations.placeholder_translations import nl_NL
+
+
+available_placeholder_translations = {
+    "it_IT":it_IT.dictionary,
+    "en_US":en_US.dictionary,
+    "nl_NL":nl_NL.dictionary,
+    "de_DE":nl_NL.dictionary,
+}
